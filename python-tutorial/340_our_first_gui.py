9a33a02012def6468e41c5c140b2ba527b04d6ca
Nuno Barreto <nbarreto@visionapartments.com>
2022-05-04 14:38:07 +0200

    Add our first gui exercise to python tutorial

---
+++ python-tutorial/340_our_first_gui.py
@@ -0,0 +1,27 @@
+picture = [
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 1, 1, 1, 0, 0],
+    [0, 1, 1, 1, 1, 1, 0],
+    [1, 1, 1, 1, 1, 1, 1],
+    [0, 0, 0, 1, 0, 0, 0],
+    [0, 0, 0, 1, 0, 0, 0],
+]
+
+for row in picture:
+    text = ''
+
+    for column in row:
+        if column:
+            text += '*'
+        else:
+            text += ' '
+
+    print(text)
+
+for row in picture:
+    for column in row:
+        if column:
+            print('*', end='')
+        else:
+            print(' ', end='')
+    print()
